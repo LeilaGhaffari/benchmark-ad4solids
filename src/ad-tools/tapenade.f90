5d3f7fec2843df4d59b8f17071a01e8c0663a896
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2024-10-30 13:29:14 -0600

    fortran functions shouldn't be in a module

--- src/ad-tools/tapenade.f90
+++ src/ad-tools/tapenade.f90
@@ -1,39 +1,30 @@
-module tapenade_module
+function strainenergy_tapenade(e_sym, lambda, mu) result(energy)
   implicit none
-contains
+  real(8), intent(in) :: e_sym(6)
+  real(8), intent(in) :: lambda, mu
+  real(8) :: energy
+  energy = sum(e_sym) * lambda * mu
+end function strainenergy_tapenade
 
-  function strainenergy_tapenade(e_sym, lambda, mu) result(energy)
-    implicit none
-    real(8), intent(in) :: e_sym(6)
-    real(8), intent(in) :: lambda, mu
-    real(8) :: energy
+subroutine kirchhofftau_tapenade(lambda, mu, e_sym, tau_sym)
+  implicit none
+  real(8), intent(in) :: lambda, mu
+  real(8), intent(in) :: e_sym(6)
+  real(8), intent(out) :: tau_sym(6)
+  integer :: i
+  do i = 1, 6
+    tau_sym(i) = (lambda + mu) * e_sym(i)
+  end do
+end subroutine kirchhofftau_tapenade
 
-    energy = sum(e_sym) * lambda * mu
-  end function strainenergy_tapenade
-
-  subroutine kirchhofftau_tapenade(lambda, mu, e_sym, tau_sym)
-    implicit none
-    real(8), intent(in) :: lambda, mu
-    real(8), intent(in) :: e_sym(6)
-    real(8), intent(out) :: tau_sym(6)
-    integer :: i
-
-    do i = 1, 6
-      tau_sym(i) = (lambda + mu) * e_sym(i)
-    end do
-  end subroutine kirchhofftau_tapenade
-
-  subroutine dtau_fwd_tapenade(lambda, mu, e_sym, de_sym, tau_sym, dtau_sym)
-    implicit none
-    real(8), intent(in) :: lambda, mu
-    real(8), intent(in) :: e_sym(6), de_sym(6)
-    real(8), intent(out) :: tau_sym(6), dtau_sym(6)
-    integer :: i
-
-    do i = 1, 6
-      tau_sym(i) = lambda * e_sym(i) + mu
-      dtau_sym(i) = 2.0 * de_sym(i)
-    end do
-  end subroutine dtau_fwd_tapenade
-
-end module tapenade_module
+subroutine dtau_fwd_tapenade(lambda, mu, e_sym, de_sym, tau_sym, dtau_sym)
+  implicit none
+  real(8), intent(in) :: lambda, mu
+  real(8), intent(in) :: e_sym(6), de_sym(6)
+  real(8), intent(out) :: tau_sym(6), dtau_sym(6)
+  integer :: i
+  do i = 1, 6
+    tau_sym(i) = lambda * e_sym(i) + mu
+    dtau_sym(i) = 2.0 * de_sym(i)
+  end do
+end subroutine dtau_fwd_tapenade
